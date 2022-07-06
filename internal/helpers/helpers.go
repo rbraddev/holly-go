bc26d88da6c30ee0f88171d44a1c559356825bce
Ryan Bradshaw <ryan@rbradshaw.dev>
2022-07-06 12:57:28 +0100

    added template helper and test

--- internal/helpers/helpers.go
+++ internal/helpers/helpers.go
@@ -1,5 +1,11 @@
 package helpers
 
+import (
+	"bytes"
+	"fmt"
+	"text/template"
+)
+
 func In[T comparable](value T, checklist []T) bool {
 	for i := range checklist {
 		if value == checklist[i] {
@@ -7,4 +13,19 @@ func In[T comparable](value T, checklist []T) bool {
 		}
 	}
 	return false
+}
+
+func ParseTemplate(f string, data map[string]any) (string, error) {
+	t, err := template.ParseFiles(fmt.Sprintf("%s", f))
+	if err != nil {
+		return "", err
+	}
+
+	var tBytes bytes.Buffer
+	err = t.Execute(&tBytes, data)
+	if err != nil {
+		return "", err
+	}
+
+	return tBytes.String(), nil
 }
---
+++ internal/helpers/helpers_test.go
@@ -0,0 +1,44 @@
+package helpers
+
+import (
+	"os"
+	"testing"
+
+	"github.com/rbraddev/holly-go/internal/assert"
+)
+
+func TestParseTemplate(t *testing.T) {
+	ParseTemplateSuccessData := map[string]any{"enable": false, "vlans": []string{"10", "20"}}
+	ParseTemplateSuccessExpResult := `
+interface vlan 10
+  shutdown
+interface vlan 20
+  shutdown`
+
+	testCases := []struct {
+		name         string
+		templateFile string
+		templateData map[string]any
+		expResult    string
+		expErr       error
+	}{
+		{"ParseTemplateSuccess", "testdata/testTemplate.tmpl", ParseTemplateSuccessData, ParseTemplateSuccessExpResult, nil},
+		{"ParseTemplateNoFile", "testdata/noFile", nil, "", os.ErrNotExist},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			templateStr, err := ParseTemplate(tc.templateFile, tc.templateData)
+
+			if tc.expErr == nil {
+				assert.NilError(t, err)
+			} else {
+				assert.ExpError(t, tc.expErr)
+				return
+			}
+
+			assert.Equal(t, templateStr, tc.expResult)
+
+		})
+	}
+}
